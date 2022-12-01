e3633ebc238d046d2c1b6a6cd79cbd088e1b2f23
pabseb22 <pabseb5626@gmail.com>
2022-11-30 20:13:50 -0500

    - Borrar Main

--- Stacks_Implementation.py
+++ Stacks_Implementation.py
@@ -1,18 +1,3 @@
 from playsound import playsound
 
-def main_function():
-    control = True
-    while control == True:
-        print("!Welcome!")
-        print("Please choose an option")
-        print("1. Play a Song")
-        print("2. Exit")
-        inp = input("Please enter your option:\n")
-        print(inp)
-        if(inp == 1):
-            playsound('./songs/001. Harry Styles - As It Was.mp3')
-            print('playing sound using  playsound')     
-        elif(inp == 2):
-            control = False
 
-main_function()
