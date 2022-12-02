2fe5cb42b8c595a87ffe26d86d17bfe3390642a9
pabseb22 <pabseb5626@gmail.com>
2022-12-02 13:22:37 -0500

    - Audio Player Ready

--- Stacks_Implementation.py
+++ Stacks_Implementation.py
@@ -1,4 +1,25 @@
-from playsound import playsound
+# In order to function it needs to be installed as: "pip3 install audioplayer" in the terminal.
+import glob
+from audioplayer import AudioPlayer
+
+def Get_Music(path):
+    for song in glob.glob(path):
+        print("Playing Song: ", song)
+        music = AudioPlayer('eddy.mp3')
+        try:
+            music.play()
+        except:
+            print("Ups copyright!")
+        print("Options:")
+        print("1. Next Song")
+        print("2. Return Home")
+        print("Please enter your option: ")
+        inp = input("--> ")
+        if(inp == '1'):
+            music.stop()
+        elif( inp == '2'):
+            break
+        
 
 def main_function():
     control = True
@@ -7,12 +28,14 @@ def main_function():
         print("Please choose an option")
         print("1. Play a Song")
         print("2. Exit")
-        inp = input("Please enter your option:\n")
-        print(inp)
-        if(inp == 1):
-            playsound('./songs/001. Harry Styles - As It Was.mp3')
-            print('playing sound using  playsound')     
-        elif(inp == 2):
+        print("Please enter your option: ")
+        inp = input("--> ")
+        if(inp == '1'):
+            Get_Music('songs/*.mp3')    
+        elif(inp == '2'):
             control = False
 
 main_function()
+
+
+
